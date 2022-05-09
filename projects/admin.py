6c123d3b6b50c5438399af080a0b37b0b1a7dbb7
Alex Chiu <alexdchiu@gmail.com>
2022-05-09 12:58:17 -1000

    Feature 12 complete

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 from projects.models import Project
-
+from tasks.models import Task
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
   pass
 
+class TaskAdmin(admin.ModelAdmin):
+  pass
+
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
