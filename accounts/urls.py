9accb731e0984aab06e4da4f03aee34c86ff51a4
Alex Chiu <alexdchiu@gmail.com>
2022-05-10 09:10:03 -1000

    made more cleanliness changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from accounts.views import SignUpView
 
 
 urlpatterns = [
-  path("login/", LoginView.as_view(), name="login"),
-  path("logout/", LogoutView.as_view(), name="logout"),
-  path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
--- projects/urls.py
+++ projects/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
+
 # from projects.models import Project
 
+
 from projects.views import (
-  ProjectListView,
-  ProjectDetailView,
-  ProjectCreateView,
+    ProjectListView,
+    ProjectDetailView,
+    ProjectCreateView,
 )
 
+
 urlpatterns = [
-  path("", ProjectListView.as_view(), name="list_projects"),
-  path("<int:pk>/", ProjectDetailView.as_view(), name="show_project"),
-  path("create/", ProjectCreateView.as_view(), name="create_project"),
+    path("", ProjectListView.as_view(), name="list_projects"),
+    path("<int:pk>/", ProjectDetailView.as_view(), name="show_project"),
+    path("create/", ProjectCreateView.as_view(), name="create_project"),
 ]
