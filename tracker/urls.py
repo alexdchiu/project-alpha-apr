11ff113528475b26c502761efa15bc597b2fffd1
Alex Chiu <alexdchiu@gmail.com>
2022-05-13 14:24:11 -1000

    Final submission

--- tracker/urls.py
+++ tracker/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include, reverse_lazy
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path(
         "",
